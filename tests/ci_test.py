804e6a793d437da7dfa4ec3fa93f1375453c543c
Akshay Kumar U <akshayapsangi123@gmail.com>
2026-02-12 14:59:52 +0530

    added headless behaviour

--- tests/ci_test.py
+++ tests/ci_test.py
@@ -2,29 +2,36 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
-import time
 
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-options = Options()
-# options.add_argument("--headless")
-# options.add_argument("--no-sandbox")
-# options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    driver.quit()
 
-file_path =  "file:///Users/akshaykumaru/Development/Indian%20Oil%20Training/CICD%20Demo/index.html"
-print(file_path)
-def test_submit_form():
-  
+def test_submit_form(driver):
+    file_path = "file://" + os.path.abspath("index.html")
+
     driver.get(file_path)
-    
+
     driver.find_element(By.ID, "name").send_keys("John Doe")
     driver.find_element(By.ID, "password").send_keys("password")
     driver.find_element(By.ID, "email").send_keys("john.doe@example.com")
     driver.find_element(By.ID, "age").send_keys("25")
     driver.find_element(By.ID, "date").send_keys("2025-01-01")
+
     driver.find_element(By.ID, "submit").click()
-    time.sleep(10)
-    assert driver.find_element(By.ID, "message").text == "Form submitted successfully"
-    print("Test passed")
-    driver.quit()
+
+    message = WebDriverWait(driver, 5).until(
+        EC.visibility_of_element_located((By.ID, "message"))
+    )
+
+    assert message.text == "Form submitted successfully"
